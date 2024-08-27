c5b35330d85f7e2d014002268e52c5b63e4ac471
kitparl <pranshu.java@gmail.com>
2024-08-27 18:07:37 +0530

    added singly linkedlist

---
+++ more/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,47 @@
+public class SinglyLinkedList {
+  class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public Node head = null;
+  public Node tail = null;
+
+  public void addNode(int data) {
+    Node newNode = new Node(data);
+
+    if (head == null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      tail = newNode;
+    }
+  }
+
+  public void print() {
+    Node current = head;
+    if (head == null) {
+      System.out.println("Empty List");
+      return;
+    }
+    while (current != null) {
+      System.out.print(current.data + " -> ");
+      current = current.next;
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    SinglyLinkedList list = new SinglyLinkedList();
+    list.addNode(1);
+    list.addNode(2);
+    list.addNode(3);
+    list.print();
+  }
+}
