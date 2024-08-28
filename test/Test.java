2f171bcefdfbfb3a83aca327611f0226281395e8
kitparl <144054850+kitparl@users.noreply.github.com>
2024-08-28 12:41:06 +0530

    Update Test.java

--- test/Test.java
+++ test/Test.java
@@ -4,6 +4,10 @@ public class Test {
   }
 
   public static void main(String[] args) {
+    // System.out.println("test");
+  }
+
+  public static void test() {
     System.out.println("test");
   }
 }
